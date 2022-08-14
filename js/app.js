22f41fe751f0d3276ebbe3b4fc7adf011b5ffbec
nazarfadillahs <nazarfadillahs@gmail.com>
2022-08-14 20:07:35 +0700

    update fitur

--- js/app.js
+++ js/app.js
@@ -19,6 +19,7 @@ const scriptURL = "https://script.google.com/macros/s/AKfycbzBfsatNeo393VAbaB_Lo
 const form = document.forms["contact"];
 const btnKirim = document.querySelector(".button");
 const btnLoading = document.querySelector(".button-loading");
+const popUp = document.querySelector(".alert");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -26,14 +27,22 @@ form.addEventListener("submit", (e) => {
   //tampilkan tombol loading
   btnLoading.classList.toggle("d-none");
   btnKirim.classList.toggle("d-none");
+
   fetch(scriptURL, { method: "POST", body: new FormData(form) })
     .then((response) => {
       //tampilkan tombol kirim setelah berhasil terkirim
       btnLoading.classList.toggle("d-none");
       btnKirim.classList.toggle("d-none");
+      popUp.classList.toggle("d-none");
       //reset
       form.reset();
       console.log("Success!", response);
     })
     .catch((error) => console.error("Error!", error.message));
+});
+
+const btnContinue = document.querySelector(".button-continue");
+
+btnContinue.addEventListener("click", function () {
+  popUp.classList.toggle("d-none");
 });
